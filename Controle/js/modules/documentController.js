2298edf3b310c16691a4619bd19b79d4454072d9
eloisecsmt <162796340+eloisecsmt@users.noreply.github.com>
2025-08-28 16:37:01 +0200

    Update documentController.js

--- Controle/js/modules/documentController.js
+++ Controle/js/modules/documentController.js
@@ -496,7 +496,7 @@ export class DocumentController {
                         }
                     },
                     {
-                        text: 'Par rapport à l\'ancien profil, les informations ont-elles bien évolué ?',
+                        text: 'Par rapport à l\'ancien profil, les informations ont-elles évolué ?',
                         type: 'boolean',
                         required: true,
                         showOnlyFor: ['MIS_A_JOUR'],
@@ -5972,6 +5972,7 @@ generateManualResultsTable(results) {
         Utils.debugLog('DocumentController réinitialisé (révisions incluses)');
     }
 }
+
 
 
 
